5545ba238e446c3d5a5dc4a4d8cabc7d6096b003
Igor-Lysak <lisakigor485@gmail.com>
2024-01-25 21:27:18 +0200

    code4you

--- script.js
+++ script.js
@@ -1,34 +1,20 @@
-// This is a simple Password Generator App that will generate random password maybe you can you them to secure your account.
-// I tried my best to make the code as simple as possible please dont mind the variable names.
-// Also this idea came in my mind after checking Traversy Media's latest video.
-
-// Clear the concole on every refresh
 console.clear();
-// set the body to full height
-// document.body.style.height = `${innerHeight}px`
-
-// Range Slider Properties.
-// Fill : The trailing color that you see when you drag the slider.
-// background : Default Range Slider Background
 const sliderProps = {
   fill: "#0B1EDF",
   background: "rgba(255, 255, 255, 0.214)",
 };
 
-// Selecting the Range Slider container which will effect the LENGTH property of the password.
 const slider = document.querySelector(".range__slider");
 
-// Text which will show the value of the range slider.
 const sliderValue = document.querySelector(".length__title");
 
-// Using Event Listener to apply the fill and also change the value of the text.
 slider.querySelector("input").addEventListener("input", (event) => {
   sliderValue.setAttribute("data-length", event.target.value);
   applyFill(event.target);
 });
-// Selecting the range input and passing it in the applyFill func.
+
 applyFill(slider.querySelector("input"));
-// This function is responsible to create the trailing color and setting the fill.
+
 function applyFill(slider) {
   const percentage =
     (100 * (slider.value - slider.min)) / (slider.max - slider.min);
@@ -132,7 +118,6 @@ copyBtn.addEventListener("click", () => {
   copiedInfo.style.opacity = "0.75";
 });
 
-// When Generate is clicked Password id generated.
 generateBtn.addEventListener("click", () => {
   const length = +lengthEl.value;
   const hasLower = lowercaseEl.checked;
